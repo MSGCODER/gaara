fc34a0561861c030c3d4b2e9100a4d1ca3f5f7cb
管世明 <guanshiming.love@163.com>
2016-09-28 18:08:53 +0800

    编写数据库sql语句

--- sqls/gaara.sql
+++ sqls/gaara.sql
@@ -8,3 +8,34 @@ CREATE TABLE client(
   description TEXT,
   is_disabled BIT DEFAULT 0
 );
+
+CREATE TABLE user(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  user_id VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(50) NOT NULL,
+  nickname VARCHAR(50) NOT NULL,
+  is_disabled BIT DEFAULT 0
+);
+
+CREATE TABLE access_token(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  client_id VARCHAR(255) NOT NULL,
+  user_id VARCHAR(50) NOT NULL,
+  access_token VARCHAR(255) NOT NULL,
+  expire_in LONG NOT NULL,
+  refresh_token VARCHAR(255) NOT NULL,
+  is_available BIT DEFAULT 0,
+  create_time DATE NOT NULL
+);
+
+CREATE TABLE auth_code(
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  client_id VARCHAR(255) NOT NULL,
+  user_id VARCHAR(50) NOT NULL,
+  auth_code VARCHAR(255) NOT NULL,
+  is_available BIT DEFAULT 0,
+  create_time DATE NOT NULL
+);
